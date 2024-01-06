9dc368a1dc66d2c12f1db8eaecd3cd718ea60664
vinze-ps <patryk.surmacz12@gmail.com>
2024-01-06 14:40:32 +0100

    next

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">VINZE</h1>
+      <Link className="mt-3" href="/admin">
+        Admin
+      </Link>
     </main>
   );
 }
---
+++ pages/admin/index.tsx
@@ -0,0 +1,49 @@
+import NoSSR from "@/components/NoSSR/NoSSR";
+import React, { useMemo, useState } from "react";
+import useLocalStorage from "use-local-storage";
+import VinzeReactAdmin from "vinze-react-admin";
+import { IVRAAuth, IVRAModule, IVRAPost, IVRAUserData } from "vinze-react-admin";
+
+const Admin = () => {
+  const [userData, setUserData] = useLocalStorage<IVRAUserData | null>("vra-user-data", null);
+  const [postsData, setPostsData] = useState<IVRAPost[]>([]);
+
+  const postsModule: IVRAModule<IVRAPost> = useMemo(
+    () => ({
+      id: "POSTS",
+      config: {
+        data: postsData,
+        content: [
+          {
+            type: "DISPLAY",
+          },
+        ],
+      },
+    }),
+    [postsData]
+  );
+
+  const modules: IVRAModule[] = useMemo(() => [postsModule], [postsModule]);
+  const baseInfo = useMemo(() => ({ companyName: "VINZE" }), []);
+
+  const auth: IVRAAuth = useMemo(
+    () => ({
+      userData,
+      config: {
+        googleAuth: true,
+      },
+      onSubmit: (data) => setTimeout(() => setUserData({ name: data.username }), 1000),
+      onSubmitGoogle: () => {},
+      onLogout: () => setUserData(null),
+    }),
+    [setUserData, userData]
+  );
+
+  return (
+    <NoSSR>
+      <VinzeReactAdmin modules={modules} auth={auth} baseInfo={baseInfo} />
+    </NoSSR>
+  );
+};
+
+export default Admin;
