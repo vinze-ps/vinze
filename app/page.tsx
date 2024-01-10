1c94e16f8b9e96bf2a6ec744863c176490f6d85f
vinze-ps <patryk.surmacz12@gmail.com>
2024-01-10 22:46:25 +0100

    next

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import styles from "@/app/page.module.scss";
+import Logo from "@/components/SVG/Logo.svg";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">VINZE</h1>
-      <Link className="mt-3" href="/admin">
+      {/* <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">VINZE</h1> */}
+      <Logo className={styles["path"]} />
+      {/* <Link className="mt-3" href="/admin">
         Admin
-      </Link>
+      </Link> */}
     </main>
   );
 }
