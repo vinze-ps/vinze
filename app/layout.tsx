2eb88749ac0f849c784efb1528c95378b6a7d8dd
Patryk Surmacz <patryk@p88.pl>
2024-02-21 10:53:58 +0100

    next

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import "@/app/globals.scss";
 import { Providers } from "@/app/providers";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import Navbar from "@/components/Navbar/Navbar";
-import Footer from "@/components/Footer/Footer";
 import PageAnimatePresence from "@/components/HOC/PageAnimatePresence";
 
 export const metadata: Metadata = {
@@ -17,11 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="pl" data-theme="DARK" className={`dark ${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
         <Providers>
-          <Navbar />
-          <PageAnimatePresence>
-            <div className="w-full mx-auto min-h-[calc(100vh-4rem)]">{children}</div>
-          </PageAnimatePresence>
-          <Footer />
+          <PageAnimatePresence>{children}</PageAnimatePresence>
         </Providers>
       </body>
     </html>
--- components/HOC/PageAnimatePresence.tsx
+++ components/HOC/PageAnimatePresence.tsx
@@ -1,16 +1,27 @@
 "use client";
-
 import { usePathname } from "next/navigation";
 import { AnimatePresence, motion } from "framer-motion";
-import FrozenRoute from "@/components/HOC/FrozenRoute";
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 
 const PageAnimatePresence = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.div key={pathname}>
-        <FrozenRoute>{children}</FrozenRoute>
+    <AnimatePresence>
+      <motion.div key={pathname} className="w-full h-full">
+        <motion.div
+          initial={{ height: "100vh" }}
+          animate={{ height: "0vh" }}
+          exit={{ height: "0vh" }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          className="fixed top-0 left-0 bg-[red] w-[100vw] h-[100vh] z-[30]"
+        ></motion.div>
+        <Navbar />
+        <div className="w-full mx-auto min-h-[calc(100vh-4rem)]">
+          <motion.main className="w-full px-4 py-8">{children}</motion.main>
+        </div>
+        <Footer />
       </motion.div>
     </AnimatePresence>
   );
