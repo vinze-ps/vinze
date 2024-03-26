e4e38f93bc2e1ba15d11f165940d09b11f880aec
vinze-ps <patryk.surmacz12@gmail.com>
2024-03-26 23:07:26 +0100

    next

--- containers/home-page/overview-section/index.tsx
+++ containers/home-page/overview-section/index.tsx
@@ -12,27 +12,39 @@ const OverviewSection = () => {
     offset: ["start end", "end start"],
   });
 
+  const yBg = useTransform(scrollYProgress, [0, 1], ["-100%", "0%"]);
   const y1 = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
   const y2 = useTransform(scrollYProgress, [0, 1], ["0%", "-140%"]);
+  const y3 = useTransform(scrollYProgress, [0, 1], ["0%", "-40%"]);
+  const y4 = useTransform(scrollYProgress, [0, 1], ["0%", "-200%"]);
 
   return (
     <div ref={containerRef} className="w-full h-[200vh] relative mb-8 md:mb-16">
-      <div></div>
-      <AnimatedSectionContainer className="mx-auto max-w-[1024px] h-[100vh] sticky overflow-hidden top-0 left-0">
-        <motion.div>
+      <div className="text-center sticky w-full h-[100vh] flex items-center justify-center flex-col top-0 left-0 z-[1]">
+        <p>My projects</p>
+        <h3 className="text-4xl font-bold tracking-tight">Overview</h3>
+      </div>
+      <AnimatedSectionContainer className="max-w-[1024px] w-full h-full absolute overflow-hidden top-0 left-[50%] translate-x-[-50%]">
+        <motion.div className="absolute left-[50%] top-[50%] w-full" style={{ y: yBg, translateX: "-50%" }}>
           <Image src={Mesh1} alt="mesh" className="w-auto mx-auto h-full opacity-[0.5]" />
         </motion.div>
-        <motion.div
-          style={{ y: y1 }}
-          className="w-[215px] h-[466px] bg-default-300 absolute left-[30px] top-[50px] rounded-xl"
-        ></motion.div>
-        <motion.div
-          style={{ y: y2 }}
-          className="w-[615px] h-[466px] bg-default-300 absolute right-[30px] top-[250px] rounded-xl"
-        ></motion.div>
-        <div className="text-center absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
-          <p>My projects</p>
-          <h3 className="text-4xl font-bold tracking-tight">Overview</h3>
+        <div className="grid grid-cols-3 w-full z-[1] relative gap-y-8">
+          <motion.div
+            style={{ y: y1 }}
+            className="w-[215px] h-[466px] bg-default-300 rounded-xl col-span-1"
+          ></motion.div>
+          <motion.div
+            style={{ y: y2 }}
+            className="w-[615px] h-[466px] bg-default-300 rounded-xl col-span-2"
+          ></motion.div>
+          <motion.div
+            style={{ y: y3 }}
+            className="w-[615px] h-[466px] bg-default-300 rounded-xl col-span-2"
+          ></motion.div>
+          <motion.div
+            style={{ y: y4 }}
+            className="w-[215px] h-[466px] bg-default-300 rounded-xl col-span-1"
+          ></motion.div>
         </div>
       </AnimatedSectionContainer>
     </div>
